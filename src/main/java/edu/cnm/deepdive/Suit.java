dd712b411d6500053a58fd914850f5284643a4b4
Chris <cgonzalez83@cnm.edu>
2024-09-26 13:47:55 -0600

    Replaced traditional class of enumerated values with enum

--- src/main/java/edu/cnm/deepdive/Suit.java
+++ src/main/java/edu/cnm/deepdive/Suit.java
@@ -1,57 +1,20 @@
 package edu.cnm.deepdive;
 
-public class Suit {
+public enum Suit {
 
-  public static final Suit CLUBS = new Suit('♣', "CLUBS");
-  public static final Suit DIAMONDS = new Suit('♢', "DIAMONDS");
-  public static final Suit HEARTS = new Suit('♡', "HEARTS");
-  public static final Suit SPADES = new Suit('♠', "SPADES");
-
-  private static final Suit [] values = {CLUBS, DIAMONDS, HEARTS, SPADES};
+  CLUBS('♣'),
+  DIAMONDS('♢'),
+  HEARTS('♡'),
+  SPADES('♠');
 
   private final char symbol;
-  private final String name;
 
-  private Suit(char symbol, String name) {
+  Suit(char symbol) {
     this.symbol = symbol;
-    this.name = name;
   }
 
   public char getSymbol() {
     return symbol;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public static Suit[] getValues() {
-      return values.clone();
-  }
-
-  public int ordinal() {
-    int position = -1;
-    for (int i = 0; i < values.length; i++) {
-      if (values[i] == this) {
-        position = i;
-        break;
-      }
-    }
-    return position;
-  }
-
-  public static Suit valueOf(String name) {
-    for (Suit suit : values) {
-      if (suit.name.equals(name)) {
-        return suit;
-      }
-    }
-    throw new IllegalArgumentException();
-  }
-
-  @Override
-  public String toString() {
-    return name;
   }
 
 }
