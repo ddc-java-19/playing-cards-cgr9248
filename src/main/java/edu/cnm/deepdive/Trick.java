f1fc009c020b1f9c6d40395d74054541a1f015a1
Chris <cgonzalez83@cnm.edu>
2024-09-30 09:32:56 -0600

    Completed trick

--- src/main/java/edu/cnm/deepdive/Trick.java
+++ src/main/java/edu/cnm/deepdive/Trick.java
@@ -3,22 +3,28 @@ package edu.cnm.deepdive;
 import edu.cnm.deepdive.model.Card;
 import edu.cnm.deepdive.model.Deck;
 import edu.cnm.deepdive.model.Suit;
+import edu.cnm.deepdive.service.ColorComparator;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class Trick {
 
+  private static final String TRICK_PILE_FORMAT = "%1$s pile: %2$s; %1$s card count = %3$d%n";
+
   public static void main(String[] args) {
     List<Card> blackPile = new ArrayList<>();
     List<Card> redPile = new ArrayList<>();
     distributeCards(createDeck(), blackPile, redPile);
-    int NumCardsToSwap = new Random().nextInt( 1 + Math.min(blackPile.size(), redPile.size()));
+    int NumCardsToSwap = new Random().nextInt(1 + Math.min(blackPile.size(), redPile.size()));
     swapCards(blackPile, redPile, NumCardsToSwap);
     int blackCount = countCardsForColor(blackPile, Suit.Color.BLACK);
     int redCount = countCardsForColor(redPile, Suit.Color.RED);
-
-    // TODO Display the contents of each pile in color order followed by the corresponding count.
+    ColorComparator comparator = new ColorComparator();
+    blackPile.sort(comparator);
+    redPile.sort(comparator);
+    displayPile(blackPile, Suit.Color.BLACK, blackCount);
+    displayPile(redPile, Suit.Color.RED, redCount);
   }
 
   private static Deck createDeck() {
@@ -38,6 +44,7 @@ public class Trick {
       }
     }
   }
+
   private static void swapCards(List<Card> blackPile, List<Card> redPile, int numCardsToSwap) {
     for (int i = 0; i < numCardsToSwap; i++) {
       blackPile.add(redPile.removeFirst());
@@ -54,4 +61,9 @@ public class Trick {
     }
     return count;
   }
+
+  private static void displayPile(List<Card> cards, Suit.Color color, int count) {
+    System.out.printf(TRICK_PILE_FORMAT, color, cards, count);
+  }
+
 }
